c6859ec19221424eafebfbe2b6d8c578ca663807
xeonliu <0@00.com>
2022-10-04 10:54:50 +0800

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,45 @@
 // [Name]               [Type]        [Port(s)]
 // Controller1          controller                    
 // Motor5               motor         5               
+// Motor6               motor         6               
+// Motor7               motor         7               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
-void print_v(motor m){
+void print_v(motor m,int row){
   //Brain Output
-  Brain.Screen.printAt(100, 120, "100%%Vmax = %3.1f rpm ",m.velocity(rpm));
+  Brain.Screen.setCursor(row,1);
+  Brain.Screen.clearLine();
+  Brain.Screen.print("Motor: V = %3.1f rpm ",m.velocity(rpm));
   //Cntroller Output
   Controller1.Screen.clearScreen();
-  Controller1.Screen.setCursor(1,1);
+  Controller1.Screen.setCursor(row,1);
   Controller1.Screen.print("%3.1f rpm",m.velocity(rpm));
 }
+
+void run(motor m,int scale){
+  m.spin(fwd,scale,rpm);
+}
+
+void movement(motor a,motor b,motor c,int FORWARD,int ROTATION){
+  run(a,FORWARD+ROTATION);
+  run(b,ROTATION-FORWARD);
+  run(c,ROTATION);
+}
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 while (1) {
-Motor5.spin(fwd,Controller1.Axis3.value(),rpm);
-print_v(Motor5);
-wait(200, msec);
+  movement(Motor5,Motor6,Motor7,Controller1.Axis3.value(),Controller1.Axis1.value());
+  print_v(Motor5,1);
+  print_v(Motor6,2);
+  print_v(Motor7,3);
+  //rotate(Motor5,Motor6,Motor7,Controller1.Axis1.value());
+  wait(200, msec);
 }
 Motor5.stop();
 }
